1e0fb8346b8b38db7c14d17ee6601cd56d54c334
Thorhallur Helgason <laddi@idega.com>
2004-12-06 20:14:51 +0000

    Constants and bundle starter for the agura intranet project.

---
+++ src/java/se/agura/AguraConstants.java
@@ -0,0 +1,29 @@
+/*
+ * $Id: AguraConstants.java,v 1.1 2004/12/06 20:14:51 laddi Exp $
+ * Created on 6.12.2004
+ *
+ * Copyright (C) 2004 Idega Software hf. All Rights Reserved.
+ *
+ * This software is the proprietary information of Idega hf.
+ * Use is subject to license terms.
+ */
+package se.agura;
+
+
+/**
+ * Last modified: $Date: 2004/12/06 20:14:51 $ by $Author: laddi $
+ * 
+ * @author <a href="mailto:laddi@idega.com">laddi</a>
+ * @version $Revision: 1.1 $
+ */
+public class AguraConstants {
+
+	public static final String GROUP_TYPE_PARISH = "parish";
+	public static final String GROUP_TYPE_PARISH_OFFICE = "parish_office";
+	public static final String GROUP_TYPE_SUPERVISOR = "supervisor";
+	public static final String GROUP_TYPE_ASSISTANTS = "assistants";
+	public static final String GROUP_TYPE_SUBSTITUTES = "substitutes";
+	public static final String GROUP_TYPE_EMPLOYEES = "employees";
+	public static final String GROUP_TYPE_MEETING = "meeting";
+	
+}
---
+++ src/java/se/agura/IWBundleStarter.java
@@ -0,0 +1,65 @@
+package se.agura;
+
+import javax.ejb.CreateException;
+import javax.ejb.FinderException;
+
+import com.idega.data.IDOLookupException;
+import com.idega.idegaweb.IWBundle;
+import com.idega.idegaweb.IWBundleStartable;
+import com.idega.user.data.GroupType;
+import com.idega.user.data.GroupTypeHome;
+
+/**
+ * <p>Title: idegaWeb</p>
+ * <p>Description: </p>
+ * <p>Copyright: Copyright (c) 2003</p>
+ * <p>Company: idega Software</p>
+ * @author <a href="laddi@idega.is">Thorhallur Helgason</a>
+ * @version 1.0
+ * Created on December 6th, 2004
+ */
+public class IWBundleStarter implements IWBundleStartable {
+
+	public void start(IWBundle starterBundle) {
+		insertStartData();
+	}
+	
+	public void stop(IWBundle starterBundle) {
+		// nothing to do
+	}
+
+	protected void insertStartData() {
+		insertGroupType(AguraConstants.GROUP_TYPE_ASSISTANTS);
+		insertGroupType(AguraConstants.GROUP_TYPE_EMPLOYEES);
+		insertGroupType(AguraConstants.GROUP_TYPE_MEETING);
+		insertGroupType(AguraConstants.GROUP_TYPE_PARISH);
+		insertGroupType(AguraConstants.GROUP_TYPE_PARISH_OFFICE);
+		insertGroupType(AguraConstants.GROUP_TYPE_SUBSTITUTES);
+		insertGroupType(AguraConstants.GROUP_TYPE_SUPERVISOR);
+	}
+	
+	private void insertGroupType(String groupType) {
+		try {
+			GroupTypeHome gtHome = (GroupTypeHome) com.idega.data.IDOLookup.getHome(GroupType.class);
+			GroupType grType;
+			try {
+				grType = gtHome.findByPrimaryKey(groupType);
+			}
+			catch (FinderException fe) {
+				try {
+					grType = gtHome.create();
+					grType.setType(groupType);
+					grType.setDescription("");
+					grType.setVisibility(true);
+					grType.store();
+				}
+				catch (CreateException ce) {
+					ce.printStackTrace();
+				}
+			}
+		}
+		catch (IDOLookupException ile) {
+			ile.printStackTrace();
+		}
+	}
+}
