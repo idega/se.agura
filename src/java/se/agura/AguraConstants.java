e5eb63859dc9184080ae7058e37866c877826171
Thorhallur Helgason <laddi@idega.com>
2005-02-15 16:04:12 +0000

    Added new user property.

--- src/java/se/agura/AguraConstants.java
+++ src/java/se/agura/AguraConstants.java
@@ -1,5 +1,5 @@
 /*
- * $Id: AguraConstants.java,v 1.3 2005/02/14 13:56:28 laddi Exp $
+ * $Id: AguraConstants.java,v 1.4 2005/02/15 16:04:12 laddi Exp $
  * Created on 6.12.2004
  *
  * Copyright (C) 2004 Idega Software hf. All Rights Reserved.
@@ -11,10 +11,10 @@ package se.agura;
 
 
 /**
- * Last modified: $Date: 2005/02/14 13:56:28 $ by $Author: laddi $
+ * Last modified: $Date: 2005/02/15 16:04:12 $ by $Author: laddi $
  * 
  * @author <a href="mailto:laddi@idega.com">laddi</a>
- * @version $Revision: 1.3 $
+ * @version $Revision: 1.4 $
  */
 public class AguraConstants {
 
@@ -28,5 +28,6 @@ public class AguraConstants {
 	public static final String GROUP_TYPE_MEETING = "meeting";
 	
 	public static final String USER_PROPERTY_WORK_HOURS = "work_hours";
+	public static final String USER_PROPERTY_COMMENTS = "comments";
 	
 }
